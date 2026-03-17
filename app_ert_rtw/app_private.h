18e23d2cd47de348da3b39e1a43a836cfa5d7c7b
seb hall <sebhalldesigns@gmail.com>
2026-03-17 23:17:34 +0000

    started simulink codegen integration

---
+++ app_ert_rtw/app_private.h
@@ -0,0 +1,30 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: app_private.h
+ *
+ * Code generated for Simulink model 'app'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Tue Mar 17 23:15:49 2026
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_app_private_h_
+#define RTW_HEADER_app_private_h_
+#include "rtwtypes.h"
+#include "app_types.h"
+#endif                                 /* RTW_HEADER_app_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */

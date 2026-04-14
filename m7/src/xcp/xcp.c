7b8891999915ea7af840d9223b81473f506874cb
seb hall <sebhalldesigns@gmail.com>
2026-04-14 18:43:38 +0100

    working DAQ

--- m7/src/xcp/xcp.c
+++ m7/src/xcp/xcp.c
@@ -16,6 +16,7 @@
 ***************************************************************/
 
 #include "xcp.h"
+#include <stdint.h>
 #include <string.h>
 
 #include <eth/eth.h>
@@ -26,6 +27,9 @@
 
 #define XCP_MAX_SESSIONS (4)
 #define XCP_TIMEOUT_US (500000000) /* 5 second timeout */
+#define XCP_MAX_DAQ_LISTS_PER_SESSION   (4)
+#define XCP_MAX_ODTS_PER_DAQ_LIST       (4)
+#define XCP_MAX_ODT_ENTRIES_PER_ODT     (8)
 
 /***************************************************************
 ** MARK: TYPEDEFS
@@ -35,8 +39,38 @@ typedef enum
 {
     XCP_SESSION_NONE,
     XCP_SESSION_DISCONNECTED,
-    XCP_SESSION_CONNECTED
+    XCP_SESSION_CONNECTED,
+    XCP_SESSION_DAQ_RUNNING
 } xcp_session_state_t;
+
+typedef struct
+{
+    uint8_t bit_offset;
+    uint8_t size;
+    uint8_t address_extension;
+    uint32_t address;
+} xcp_odt_entry_t;
+
+typedef struct
+{
+    uint32_t num_entries;
+    xcp_odt_entry_t entries[XCP_MAX_ODT_ENTRIES_PER_ODT];
+} xcp_odt_t;
+
+typedef struct
+{
+    uint32_t num_odts;
+    xcp_odt_t odts[XCP_MAX_ODTS_PER_DAQ_LIST];
+
+    uint8_t mode;
+    uint16_t event_channel_num;
+    uint8_t prescaler;
+    uint8_t priority;
+
+    uint32_t last_transmit_us;
+
+    uint8_t start_stop; /* 0=stopped, 1=started */
+} xcp_daq_list_t;
 
 typedef struct
 {
@@ -54,6 +88,17 @@ typedef struct
 
     bool block_upload_in_progress; /* unused for now */
     bool block_download_in_progress; /* unused for now */
+
+    xcp_daq_list_t daq_lists[XCP_MAX_DAQ_LISTS_PER_SESSION];
+    uint32_t daq_lists_in_use;
+
+    /* as assigned by SET_DAQ_PTR */
+    uint8_t active_daq_list; 
+    uint8_t active_odt;
+    uint8_t active_odt_entry;
+
+    uint8_t daq_mode;
+
 } xcp_session_t;
 
 /***************************************************************
@@ -75,6 +120,7 @@ static void close_connection(xcp_session_t *session);
 static void pack_conn_response(xcp_frame_t *response);
 static void pack_get_status_response(xcp_frame_t *response);
 static void pack_generic_response(xcp_frame_t *response); /* generic OK response */
+static void pack_start_stop_daq_list_response(xcp_frame_t *response, uint8_t first_pid);
 static void pack_error_response(xcp_frame_t *response, uint8_t error_code);
 
 static void pack_upload_response(xcp_session_t *session, xcp_frame_t *response);
@@ -190,6 +236,31 @@ void xcp_update()
 
             process_new_frame(&sessions[i]);
         }
+
+
+        if (sessions[i].state == XCP_SESSION_DAQ_RUNNING)
+        {
+            for (uint32_t daq_list_num = 0; daq_list_num < sessions[i].daq_lists_in_use; daq_list_num++)
+            {
+                xcp_daq_list_t *daq_list = &sessions[i].daq_lists[daq_list_num];
+
+                if (daq_list->start_stop == 2 && (now_us - daq_list->last_transmit_us >= (uint32_t)daq_list->prescaler * 1000U * 100))
+                {
+                    xcp_frame_t daq_frame;
+                    daq_frame.length = 2; /* PID only for now, expand as needed */
+                    daq_frame.pid = 0x00; /* Placeholder PID, set as needed */
+                    daq_frame.data[0] = 0x00; /* Placeholder data, set as needed */
+
+                    daq_list->last_transmit_us = now_us;
+
+                    response_handlers[sessions[i].conn_type](&sessions[i].conn_info, &daq_frame);
+
+                }
+            }
+
+            
+
+        }
     }
 }
 
@@ -225,6 +296,12 @@ static void process_new_frame(xcp_session_t *session)
                 case XCP_COMMAND_GET_STATUS:
                 {
                     pack_get_status_response(&response_frame);
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_SYNCH:
+                {
+                    pack_error_response(&response_frame, XCP_ERR_CMD_SYNCH);
                     handled = true;
                 } break;
 
@@ -272,18 +349,248 @@ static void process_new_frame(xcp_session_t *session)
                     handled = true;
                 } break;
 
-                case XCP_COMMAND_SYNCH:
+
+                case XCP_COMMAND_FREE_DAQ:
                 {
-                    pack_error_response(&response_frame, XCP_ERR_CMD_SYNCH);
+                    session->daq_lists_in_use = 0;
+
+                    /* actually clear entries */
+                    memset(session->daq_lists, 0, sizeof(session->daq_lists));
+
+                    pack_generic_response(&response_frame);
                     handled = true;
                 } break;
 
+                case XCP_COMMAND_ALLOC_DAQ:
+                {
+                    uint16_t requested_lists = session->frame.data[2] << 8 | session->frame.data[1];
+                    
+                    if (requested_lists > XCP_MAX_DAQ_LISTS_PER_SESSION)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_MEMORY_OVERFLOW);
+                    }
+                    else
+                    {
+                        session->daq_lists_in_use = requested_lists;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+
+                } break;
+
+                case XCP_COMMAND_ALLOC_ODT:
+                {
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    uint8_t odt_count = session->frame.data[3];
+
+                    if (daq_list_num >= session->daq_lists_in_use || odt_count > XCP_MAX_ODTS_PER_DAQ_LIST)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->daq_lists[daq_list_num].num_odts = odt_count;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+                    
+                } break;
+
+                case XCP_COMMAND_ALLOC_ODT_ENTRY:
+                {
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    uint8_t odt_num = session->frame.data[3];
+                    uint8_t entry_count = session->frame.data[4];
+
+                    if (daq_list_num >= session->daq_lists_in_use || 
+                        odt_num >= session->daq_lists[daq_list_num].num_odts || 
+                        entry_count > XCP_MAX_ODT_ENTRIES_PER_ODT)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->daq_lists[daq_list_num].odts[odt_num].num_entries = entry_count;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_SET_DAQ_PTR:
+                {
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    uint8_t odt_num = session->frame.data[3];
+                    uint8_t odt_entry_num = session->frame.data[4];
+
+                    if (daq_list_num >= session->daq_lists_in_use || 
+                        odt_num >= session->daq_lists[daq_list_num].num_odts || 
+                        odt_entry_num >= session->daq_lists[daq_list_num].odts[odt_num].num_entries)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->active_daq_list = daq_list_num;
+                        session->active_odt = odt_num;
+                        session->active_odt_entry = odt_entry_num;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+
+                } break;
+
+                case XCP_COMMAND_WRITE_DAQ:
+                {
+                    if (session->active_daq_list >= session->daq_lists_in_use || 
+                        session->active_odt >= session->daq_lists[session->active_daq_list].num_odts || 
+                        session->active_odt_entry >= session->daq_lists[session->active_daq_list].odts[session->active_odt].num_entries)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        xcp_odt_entry_t *entry = &session->daq_lists[session->active_daq_list].odts[session->active_odt].entries[session->active_odt_entry];
+                        
+                        entry->bit_offset = session->frame.data[0];
+                        entry->size = session->frame.data[1];
+                        entry->address_extension = session->frame.data[2];
+
+                        entry->address = 0;
+                        entry->address |= session->frame.data[3];
+                        entry->address |= ((uint32_t)session->frame.data[4]) << 8;
+                        entry->address |= ((uint32_t)session->frame.data[5]) << 16;
+                        entry->address |= ((uint32_t)session->frame.data[6]) << 24;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_SET_DAQ_LIST_MODE:
+                {
+                    uint8_t mode = session->frame.data[0];
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    uint16_t event_channel_num = session->frame.data[4] << 8 | session->frame.data[3];
+                    uint8_t prescaler = session->frame.data[5];
+                    uint8_t priority = session->frame.data[6];
+
+                    if (daq_list_num >= session->daq_lists_in_use)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->daq_lists[daq_list_num].mode = mode;
+                        session->daq_lists[daq_list_num].event_channel_num = event_channel_num;
+                        session->daq_lists[daq_list_num].prescaler = prescaler;
+                        session->daq_lists[daq_list_num].priority = priority;
+
+                        pack_generic_response(&response_frame);
+                    }
+
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_START_STOP_DAQ_LIST:
+                {
+                    uint8_t start_stop = session->frame.data[0];
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    
+                    if (daq_list_num >= session->daq_lists_in_use)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->daq_lists[daq_list_num].start_stop = start_stop;
+
+                        /* MATLAB expects START_STOP_DAQ_LIST response to include first PID. */
+                        pack_start_stop_daq_list_response(&response_frame, 0x00);
+                    }
+
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_START_STOP_SYNCH:
+                {
+                    uint8_t start_stop = session->frame.data[0];
+                    
+                    session->daq_mode = start_stop;
+
+                    if (start_stop == 1)
+                    {
+                        session->state = XCP_SESSION_DAQ_RUNNING;
+                        eth_log("DAQ started");
+                    }
+
+                    pack_generic_response(&response_frame);
+
+                    handled = true;
+                } break;
 
                 default:
                 {
                     /* do nothing */
                 } break;
             }
+        } break;
+
+        case XCP_SESSION_DAQ_RUNNING:
+        {
+            switch (session->frame.pid)
+            {
+                case XCP_COMMAND_START_STOP_DAQ_LIST:
+                {
+                    uint8_t start_stop = session->frame.data[0];
+                    uint16_t daq_list_num = session->frame.data[2] << 8 | session->frame.data[1];
+                    
+                    if (daq_list_num >= session->daq_lists_in_use)
+                    {
+                        pack_error_response(&response_frame, XCP_ERR_OUT_OF_RANGE);
+                    }
+                    else
+                    {
+                        session->daq_lists[daq_list_num].start_stop = start_stop;
+
+                        /* MATLAB expects START_STOP_DAQ_LIST response to include first PID. */
+                        pack_start_stop_daq_list_response(&response_frame, 0x00);
+                    }
+
+                    handled = true;
+                } break;
+
+                case XCP_COMMAND_START_STOP_SYNCH:
+                {
+                    uint8_t start_stop = session->frame.data[0];
+                    
+                    session->daq_mode = start_stop;
+
+                    if (start_stop == 1)
+                    {
+                        session->state = XCP_SESSION_DAQ_RUNNING;
+                        eth_log("DAQ started");
+                    }
+
+                    pack_generic_response(&response_frame);
+
+                    handled = true;
+                } break;
+
+                default:
+                {
+
+                } break;
+            }
+            
         } break;
 
         default:
@@ -308,7 +615,7 @@ static void process_new_frame(xcp_session_t *session)
         eth_log_u32("Invalid XCP PID", session->frame.pid);
         eth_log_u32("For state", session->state);
 
-        pack_error_response(&response_frame, 0x01); /* Generic error code, expand as needed */
+        pack_error_response(&response_frame, XCP_ERR_CMD_UNKNOWN); /* Generic error code, expand as needed */
 
     }
 
@@ -328,7 +635,7 @@ static void pack_conn_response(xcp_frame_t *response)
     response->pid = XCP_RESPONSE_OK;
     response->data[0] = 0x00; /* RESOURCE */
     response->data[0] |= 1 << XCP_CONN_RESP_RESOURCE_CAL_BIT; /* CAL resource available */
-    response->data[0] |= 0 << XCP_CONN_RESP_RESOURCE_DAQ_BIT; /* DAQ resource NOT available */
+    response->data[0] |= 1 << XCP_CONN_RESP_RESOURCE_DAQ_BIT; /* DAQ resource NOT available */
     response->data[0] |= 0 << XCP_CONN_RESP_RESOURCE_STIM_BIT; /* STIM resource NOT available */
     response->data[0] |= 0 << XCP_CONN_RESP_RESOURCE_PGM_BIT; /* PGM resource NOT available */
 
@@ -363,6 +670,13 @@ static void pack_generic_response(xcp_frame_t *response)
 {
     response->length = 1;
     response->pid = XCP_RESPONSE_OK;
+}
+
+static void pack_start_stop_daq_list_response(xcp_frame_t *response, uint8_t first_pid)
+{
+    response->length = 2;
+    response->pid = XCP_RESPONSE_OK;
+    response->data[0] = first_pid;
 }
 
 static void pack_upload_response(xcp_session_t *session, xcp_frame_t *response)
--- xcp_udp_test.m
+++ xcp_udp_test.m
@@ -1,10 +1,59 @@
 a2l_obj = xcpA2L('test.a2l');
 xcp_ch = xcpChannel(a2l_obj, 'UDP', '192.168.1.50', 5005);
 
+if ~isempty(a2l_obj.Warnings)
+    disp('A2L parser warnings:');
+    disp(a2l_obj.Warnings);
+end
+
+disp('A2L events parsed by MATLAB:');
+disp(a2l_obj.Events);
+
 connect(xcp_ch);
 
 eng = readMeasurement(xcp_ch, "bButtonState")
 
 writeCharacteristic(xcp_ch, "bLedState", uint8(1))
+
+try
+    if isempty(a2l_obj.Events)
+        error("No events parsed from A2L.");
+    end
+    events = string(a2l_obj.Events);
+    if any(events == "100 ms")
+        eventName = "100 ms";
+    else
+        eventName = events(1);
+    end
+    fprintf("Using event: %s\n", eventName);
+    createMeasurementList(xcp_ch, "DAQ", eventName, ["bButtonState"]);
+catch ME
+    fprintf("createMeasurementList failed: %s\n", ME.message);
+    fprintf("DAQ setup skipped.\n");
+    disconnect(xcp_ch);
+    return;
+end
+
+startMeasurement(xcp_ch);
+pause(0.5);
+
+try
+    daqData = readDAQ(xcp_ch, "bButtonState");
+    fprintf("readDAQ path used, class=%s\n", class(daqData));
+    disp(daqData);
+catch ME
+    fprintf("readDAQ failed: %s\n", ME.message);
+    daqData = readDAQListData(xcp_ch);
+    fprintf("readDAQListData path used, class=%s\n", class(daqData));
+    disp(daqData);
+
+    if isa(daqData, 'uint8') && numel(daqData) == 8
+        asDouble = typecast(uint8(daqData), 'double');
+        fprintf("readDAQListData interpreted as IEEE754 double: %g\n", asDouble);
+    end
+end
+
+stopMeasurement(xcp_ch);
+freeMeasurementLists(xcp_ch);
 
 disconnect(xcp_ch);
